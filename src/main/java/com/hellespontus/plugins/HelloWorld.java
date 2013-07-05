9cb3f149a1bae6a8ee9f21eb241b20aea92df157
Pavel Naydenov <naydenov.p.v@gmail.com>
2013-07-06 00:19:04 +0400

    funny effect

--- src/main/java/com/hellespontus/plugins/HelloWorld.java
+++ src/main/java/com/hellespontus/plugins/HelloWorld.java
@@ -1,17 +1,31 @@
 package com.hellespontus.plugins;
 
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
-public class HelloWorld extends JavaPlugin{
+public class HelloWorld extends JavaPlugin implements Listener{
     Logger log = Logger.getLogger("Minecraft");
 
     public void onEnable(){
-        log.info("Your plugin has been enabled!");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     public void onDisable(){
-        log.info("Your plugin has been disabled.");
+        
+    }
+    
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event){
+        if (event.getBlock().getType() == Material.GLASS) {
+            event.getPlayer().playSound(event.getBlock().getLocation(), Sound.SPIDER_WALK, 1, 1);
+            event.getBlock().getLocation().getWorld().spawnEntity(event.getBlock().getLocation(), EntityType.BAT);
+        }
     }
 }
